724cde088ca20f73758c04e2dee80d6c553d2ffd
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-23 02:19:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ActionController::Base
   
   def create
     @student = Student.new(student_params)
-    
+    @student.save
+    redirect_to student_path(@student)
   end 
 end 
