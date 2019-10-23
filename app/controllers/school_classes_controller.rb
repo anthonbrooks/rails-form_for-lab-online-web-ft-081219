7fe06608b7212ce0434c556781936652c7b74787
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-23 02:05:30 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ class SchoolClassesController < ActionController::Base
   end 
   
   def create 
-    @school_class = SchoolClass.new(school_class_params(title: , room_number))
+    @school_class = SchoolClass.new(school_class_params)
     @school_class.save
     redirect_to school_class_path(@school_class)
   end 
