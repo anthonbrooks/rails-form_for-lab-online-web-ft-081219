bb70873e6e7b3063b788935b7e32ae842a4901f0
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-23 01:57:54 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,9 @@
 class SchoolClassesController < ActionController::Base
+  def new 
+    @school_class = SchoolClass.new 
+  end 
+  
+  def show 
+    @school_class = SchoolClass.find(params[:id])
+  end 
 end 
